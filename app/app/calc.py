ead5abf75a80c909d3353c5d15079c2a4bec9c2a
Harshit Mishra <harshitmishra.84162@gmail.com>
2026-09-23 16:40:47 +0530

    latest

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,13 +1,16 @@
 """
+
 calculator fucntion
+
 """
 
-def add(x,y):
+
+def add(x, y):
     """add x and y and return the result"""
 
     return x+y
 
-def subtract(x,y):
+
+def subtract(x, y):
 
     return x-y
-
--- app/app/tests.py
+++ app/app/tests.py
@@ -6,18 +6,19 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """test the calc module"""
 
     def test_add_numbers(self):
-        """ test adding numbers together"""
+        """ test adding numbers together """
 
-        res =calc.add(5,6)
-        self.assertEqual(res,11)
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        """ test subtracting numbers together"""
+        """ test subtracting numbers together """
 
-        res = calc.subtract(11,6)
+        res = calc.subtract(11, 6)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
