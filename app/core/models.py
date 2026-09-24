d33b887f1463da82019fab77f9b24fd38630c6e1
Harshit Mishra <harshitmishra.84162@gmail.com>
2026-09-24 17:29:28 +0530

    latest

--- app/core/models.py
+++ app/core/models.py
@@ -17,12 +17,24 @@ class UserManager(BaseUserManager):   # to manage users in the system
     def create_user(self, email, password=None, **extra_field):
         """"create and save a new user"""
 
-        user=self.model(email=email, **extra_field)
+        if not email:
+            raise ValueError('user must have an email address')
+
+        user=self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self.db)
 
 
+        return user
 
+    def create_superuser(self, email, password):
+        "create and save a new superuser"
+
+        user= self.create_user(email, password)
+
+        user.is_staff= True
+        user.is_superuser = True
+        user.save(using=self.db)
 
         return user
 
@@ -40,5 +52,5 @@ class User(AbstractBaseUser, PermissionsMixin):  # to represent a user in the sy
 
     objects = UserManager()
 
-    
+
     USERNAME_FIELD='email'
