153f09f139cca36095b3a0dda63f1518cb2e6b46
Harshit Mishra <harshitmishra.84162@gmail.com>
2026-09-24 14:53:24 +0530

    latest

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,44 @@
-from django.db import models   # noqa
+"""
+database models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+
+class UserManager(BaseUserManager):   # to manage users in the system
+    """manager for users"""
+
+    def create_user(self, email, password=None, **extra_field):
+        """"create and save a new user"""
+
+        user=self.model(email=email, **extra_field)
+        user.set_password(password)
+        user.save(using=self.db)
+
+
+
+
+        return user
+
+
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):  # to represent a user in the system
+    """user in the system"""
+
+    email=models.EmailField(max_length=255, unique=True)
+    name= models.CharField(max_length=255)
+    is_active= models.BooleanField(default=True)
+    is_staff= models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    
+    USERNAME_FIELD='email'
