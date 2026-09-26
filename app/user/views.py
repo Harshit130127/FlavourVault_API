a22e17b5c5ae51b946a4cc27bf854893840c55c1
Harshit Mishra <harshitmishra.84162@gmail.com>
2026-09-26 17:22:01 +0530

    User API completed

--- app/user/views.py
+++ app/user/views.py
@@ -21,3 +21,15 @@ class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """ manage the authenticated user"""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """ retrieve and return authenticated user"""
+        return self.request.user
+
